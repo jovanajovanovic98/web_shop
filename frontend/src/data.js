dd9bda9c1b09415838ebcc2af09cefff04f08bda
jovanajovanovic98 <58151226+jovanajovanovic98@users.noreply.github.com>
2023-04-29 16:59:16 +0200

    Create List of products

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: 'Samsung Galaxy S22 128 GB - Crni',
+      slug: 'Samsung-Galaxy-S22-128-GB-Crni',
+      category: 'mobilni telefon',
+      image: '/images/sgs22c.jpg', //679x829
+      price: 119900,
+      countInStock: 10,
+      brand: 'Samsung',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Moderan dizajn pametnog telefona sa preglednim Dynamic AMOLED 2X ekranom od 6,1"',
+    },
+    {
+      name: 'iPhone 14 Pro - Space Black',
+      slug: 'iPhone-14-Pro-Space-Black',
+      category: 'mobilni telefon',
+      image: '/images/i14pb.jpg',
+      price: 157900,
+      countInStock: 8,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 15,
+      description:
+        'Vrhunske perfomanse smart telefona modernog dizajna u crnoj boji',
+    },
+    {
+      name: 'LG Smart televizor 50NANO763QA',
+      slug: 'LG-Smart-televizor-50NANO763QA',
+      category: 'TV',
+      image: '/images/lgtv50NANO763QA.jpg',
+      price: 119200,
+      countInStock: 9,
+      brand: 'Lenovo',
+      rating: 4.5,
+      numReviews: 12,
+      description: 'Smart televizor sa ekranom NanoCell tehnologije od 50"',
+    },
+    {
+      name: 'Samsung Smart televizor UE55BU8072UXXH',
+      slug: 'Samsung-Smart-televizor-UE55BU8072UXXH',
+      category: 'TV',
+      image: '/images/stvUE55BU8072UXXH.jpg',
+      price: 102290,
+      countInStock: 14,
+      brand: 'Samsung',
+      rating: 4.5,
+      numReviews: 11,
+      description:
+        'LED tehnologija ekrana dijagonale 55" sa dinamičkim kristalnim bojama',
+    },
+  ],
+};
+export default data;
